f67ae7748e34afb8036ff73e34adb5520e3496fa
Faker0715 <949506563@qq.com>
2023-05-08 13:02:57 +0800

    [feat] The UI handles the results of the camera startup && Define the universal MediaFrame structure

---
+++ xrtc/media/base/media_frame.h
@@ -0,0 +1,73 @@
+//
+// Created by faker on 2023/5/8.
+//
+
+#ifndef XRTCSDK_MEDIA_FRAME_H
+#define XRTCSDK_MEDIA_FRAME_H
+
+#include <cstring>
+#include <cstdint>
+
+namespace xrtc {
+
+    enum class MainMediaType {
+        kMainTypeCommon,
+        kMainTypeAudio,
+        kMainTypeVideo,
+        kMainTypeData,
+    };
+    enum class SubMediaType {
+        kSubTypeCommon,
+        kSubTypeI420
+    };
+    struct AudioFormat {
+        SubMediaType type;
+    };
+    struct VideoFormat {
+        SubMediaType type;
+        int width;
+        int height;
+        bool idr = false;
+    };
+
+    class MediaFormat {
+    public:
+        MainMediaType main_type;
+        union {
+            VideoFormat video_fmt;
+            AudioFormat audio_fmt;;
+        } sub_fmt;
+    };
+
+    class MediaFrame {
+    public:
+        MediaFrame(int size) : max_size(size) {
+            memset(data, 0, sizeof(data));
+            memset(data_len, 0, sizeof(data_len));
+            memset(stride, 0, sizeof(stride));
+            data[0] = new char[size];
+            data_len[0] = size;
+        };
+
+        ~MediaFrame(){
+            if(data[0]){
+                delete[] data[0];
+                data[0] = nullptr;
+            }
+        }
+
+
+    public:
+        int max_size;
+        MediaFormat fmt;
+        // 4个平面
+        char *data[4];
+        // 每个平面的字节数
+        int data_len[4];
+        int stride[4];
+        uint32_t ts = 0;
+    };
+}
+
+
+#endif //XRTCSDK_MEDIA_FRAME_H
