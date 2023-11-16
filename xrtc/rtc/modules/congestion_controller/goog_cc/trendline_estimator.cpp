b912cf946f30fdf578227f52d1978960f1d5e6f8
fangzhe <fangzhe@viture.com>
2023-11-16 18:11:57 +0800

    [feat] Linear regression minimum secondary multiplication sample data

--- xrtc/rtc/modules/congestion_controller/goog_cc/trendline_estimator.cpp
+++ xrtc/rtc/modules/congestion_controller/goog_cc/trendline_estimator.cpp
@@ -1,20 +1,72 @@
 ﻿#include "xrtc/rtc/modules/congestion_controller/goog_cc/trendline_estimator.h"
 
 namespace xrtc {
+    namespace {
 
-xrtc::TrendlineEstimator::TrendlineEstimator() {
-}
+        const double kDefaultTrendlineSmoothingCoef = 0.9;
+        const size_t kDefaultTrendlineWindowSize = 20;
 
-TrendlineEstimator::~TrendlineEstimator() {
-}
+    } // namespace
 
-void TrendlineEstimator::Update(webrtc::TimeDelta recv_time_delta, 
-    webrtc::TimeDelta send_time_delta, 
-    webrtc::Timestamp send_time, 
-    webrtc::Timestamp arrival_time, 
-    size_t packet_size, 
-    bool calculated_delta) 
-{
-}
+    xrtc::TrendlineEstimator::TrendlineEstimator() :
+            smoothing_coef_(kDefaultTrendlineSmoothingCoef)
+    {
+    }
+
+    TrendlineEstimator::~TrendlineEstimator() {
+    }
+
+    void TrendlineEstimator::Update(webrtc::TimeDelta recv_time_delta,
+                                    webrtc::TimeDelta send_time_delta,
+                                    webrtc::Timestamp send_time,
+                                    webrtc::Timestamp arrival_time,
+                                    size_t packet_size,
+                                    bool calculated_delta)
+    {
+        if (calculated_delta) {
+            UpdateTrendline(recv_time_delta.ms<double>(),
+                            send_time_delta.ms<double>(),
+                            send_time.ms(), arrival_time.ms(),
+                            packet_size);
+        }
+    }
+
+    void TrendlineEstimator::UpdateTrendline(double recv_delta_ms,
+                                             double send_delta_ms,
+                                             int64_t send_time_ms,
+                                             int64_t arrival_time_ms,
+                                             size_t packet_size)
+    {
+        if (-1 == first_arrival_time_ms_) {
+            first_arrival_time_ms_ = arrival_time_ms;
+        }
+
+        // 计算传输的延迟差
+        double delta_ms = recv_delta_ms - send_delta_ms;
+        accumulated_delay_ms_ += delta_ms;
+        // 计算指数平滑后的累计延迟差
+        smoothed_delay_ms_ = smoothing_coef_ * smoothed_delay_ms_ +
+                             (1 - smoothing_coef_) * accumulated_delay_ms_;
+        // 将样本数据添加到队列
+        delay_hist_.emplace_back(
+                static_cast<double>(arrival_time_ms - first_arrival_time_ms_),
+                smoothed_delay_ms_, accumulated_delay_ms_);
+
+        if (delay_hist_.size() > 20) {
+            delay_hist_.pop_front();
+        }
+
+        // 当样本数据满足要求，计算trend值
+        double trend = prev_trend_;
+        if (delay_hist_.size() == kDefaultTrendlineWindowSize) {
+            trend = LinearFitSlope(delay_hist_).value_or(trend);
+        }
+    }
+
+    absl::optional<double> TrendlineEstimator::LinearFitSlope(
+            const std::deque<PacketTiming>& packets)
+    {
+        return absl::optional<double>();
+    }
 
 } // namespace xrtc
--- xrtc/rtc/modules/congestion_controller/goog_cc/trendline_estimator.h
+++ xrtc/rtc/modules/congestion_controller/goog_cc/trendline_estimator.h
@@ -1,23 +1,58 @@
 ﻿#ifndef XRTCSDK_XRTC_RTC_MODULES_CONGESTION_CONTROLLER_GOOG_CC_TRENDLINE_ESTIMATOR_H_
 #define XRTCSDK_XRTC_RTC_MODULES_CONGESTION_CONTROLLER_GOOG_CC_TRENDLINE_ESTIMATOR_H_
 
+#include <deque>
+
+#include <absl/types/optional.h>
 #include <api/units/time_delta.h>
 #include <api/units/timestamp.h>
 
 namespace xrtc {
 
-class TrendlineEstimator {
-public:
-    TrendlineEstimator();
-    ~TrendlineEstimator();
+    class TrendlineEstimator {
+    public:
+        TrendlineEstimator();
+        ~TrendlineEstimator();
 
-    void Update(webrtc::TimeDelta recv_time_delta,
-        webrtc::TimeDelta send_time_delta,
-        webrtc::Timestamp send_time,
-        webrtc::Timestamp arrival_time,
-        size_t packet_size,
-        bool calculated_delta);
-};
+        void Update(webrtc::TimeDelta recv_time_delta,
+                    webrtc::TimeDelta send_time_delta,
+                    webrtc::Timestamp send_time,
+                    webrtc::Timestamp arrival_time,
+                    size_t packet_size,
+                    bool calculated_delta);
+
+        struct PacketTiming {
+            PacketTiming(double arrival_time_ms,
+                         double smoothed_delay_ms,
+                         double raw_delay_ms) :
+                    arrival_time_ms(arrival_time_ms),
+                    smoothed_delay_ms(smoothed_delay_ms),
+                    raw_delay_ms(raw_delay_ms) { }
+
+            double arrival_time_ms; // 数据包到达的时间
+            double smoothed_delay_ms; // 指数平滑之后的延迟差
+            double raw_delay_ms; // 原始的延迟差
+        };
+
+    private:
+        void UpdateTrendline(double recv_delta_ms,
+                             double send_delta_ms,
+                             int64_t send_time_ms,
+                             int64_t arrival_time_ms,
+                             size_t packet_size);
+
+        absl::optional<double> LinearFitSlope(
+                const std::deque<PacketTiming>& packets);
+
+    private:
+        int64_t first_arrival_time_ms_ = -1;
+        double accumulated_delay_ms_ = 0;
+        double smoothed_delay_ms_ = 0;
+        // 表示了历史数据的权重
+        double smoothing_coef_;
+        std::deque<PacketTiming> delay_hist_;
+        double prev_trend_ = 0.0f;
+    };
 
 } // namespace xrtc
 
